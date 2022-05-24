5b0f88f880ac7ac5a06d69593a49d46fd861cdda
ernie.vanduyne <ernie.vanduyne@biworldwide.com>
2022-05-24 13:45:19 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload ERNIE.
         </p>
         <a
           className="App-link"
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p className="App-intro">
+        Hola from Ernie
+      </p>
     </div>
   );
 }
